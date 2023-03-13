90dbea06dbf6945c0d163acb6044e526fc013dec
jorandmol <jorandmol.dev@gmail.com>
2023-03-13 20:09:09 +0100

    Add config files and common entity

---
+++ src/app/models/entity.model.ts
@@ -0,0 +1,16 @@
+export class Entity {
+    private _id: string;
+    private _version: number;
+
+    constructor() {
+        this._id = '0';
+        this._version = 0;
+    }
+
+    public get id(): string {
+        return this._id;
+    }
+    public get version(): number {
+        return this._version;
+    }
+}
