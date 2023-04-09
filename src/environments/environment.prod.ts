948e5e4cc41552fbf517a60f47eee54831de978a
jorandmol <jorandmol.dev@gmail.com>
2023-04-09 20:51:36 +0200

    Configure (not working) i18n compilation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  backendApiBaseUrl: 'https://acme-explorer-u21d.onrender.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyBEyOBgsFHIbPKdSRV8YGrnZguUHGpu1rE",
+    authDomain: "acme-explorer-3300e.firebaseapp.com",
+    projectId: "acme-explorer-3300e",
+    storageBucket: "acme-explorer-3300e.appspot.com",
+    messagingSenderId: "248517529315",
+    appId: "1:248517529315:web:9447f77fb93ca70c807fd6"
+  }
 };
