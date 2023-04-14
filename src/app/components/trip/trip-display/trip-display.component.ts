4f407eab1e6cc96d36e34d5ba865ccaa5e511c04
Juan Carlos Cortés Muñoz <juacormun@alum.us.es>
2023-04-14 09:34:30 +0200

    Add random banner in trip display

--- src/app/components/trip/trip-display/trip-display.component.ts
+++ src/app/components/trip/trip-display/trip-display.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Role } from 'src/app/enums/RoleEnum';
 import { Actor } from 'src/app/models/actor';
 import { Picture } from 'src/app/models/picture';
+import { Sponsorship } from 'src/app/models/sponsorship';
 import { Trip } from 'src/app/models/trip';
 import { AuthService } from 'src/app/services/auth.service';
 import { TripService } from 'src/app/services/trip.service';
@@ -16,6 +17,7 @@ export class TripDisplayComponent implements OnInit {
 
   id: string;
   trip: Trip;
+  sponsorship: any;
   actor: Actor;
 
   constructor(
@@ -33,6 +35,7 @@ export class TripDisplayComponent implements OnInit {
     this.id = this.route.snapshot.params['id'];
     this.tripService.getTrip(this.id).subscribe((trip) => {
       this.trip = trip;
+      this.sponsorship = this.getRandomSponsorship();
     })
     this.actor = this.authService.getCurrentActor();
   }
@@ -43,6 +46,10 @@ export class TripDisplayComponent implements OnInit {
 
   getMainPicture(): Picture {
     return this.trip.pictures?.length > 0 ? this.trip.pictures[0] : new Picture()
+  }
+
+  getRandomSponsorship(): Sponsorship | null {
+    return this.trip.sponsorships?.length > 0 ? this.trip.sponsorships[Math.floor(Math.random() * this.trip.sponsorships.length)] : null;
   }
 
   canDisplayActions(): boolean {
