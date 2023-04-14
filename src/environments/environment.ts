08ac9402887b0fbb2bf1eb5b0d93627a48c57c25
Juan Carlos Cortés Muñoz <juacormun@alum.us.es>
2023-04-14 23:36:59 +0200

    Add translations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  backendApiBaseUrl: 'http://localhost:8080',
+  backendApiBaseUrl: 'https://acme-explorer-u21d.onrender.com',
   firebaseConfig: {
     apiKey: "AIzaSyBEyOBgsFHIbPKdSRV8YGrnZguUHGpu1rE",
     authDomain: "acme-explorer-3300e.firebaseapp.com",
@@ -14,6 +13,19 @@ export const environment = {
     appId: "1:248517529315:web:9447f77fb93ca70c807fd6"
   }
 };
+
+// export const environment = {
+//   production: false,
+//   backendApiBaseUrl: 'http://localhost:8080',
+//   firebaseConfig: {
+//     apiKey: "AIzaSyBEyOBgsFHIbPKdSRV8YGrnZguUHGpu1rE",
+//     authDomain: "acme-explorer-3300e.firebaseapp.com",
+//     projectId: "acme-explorer-3300e",
+//     storageBucket: "acme-explorer-3300e.appspot.com",
+//     messagingSenderId: "248517529315",
+//     appId: "1:248517529315:web:9447f77fb93ca70c807fd6"
+//   }
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
