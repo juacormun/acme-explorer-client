d25d33e8025fd83b58698dfe8283542d48df4ec8
jorandmol <jorandmol.dev@gmail.com>
2023-05-01 14:45:21 +0200

    Fix registration form #14

--- src/app/components/security/register/register.component.ts
+++ src/app/components/security/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MessageType } from 'src/app/enums/MessageEnum';
 import { Role } from 'src/app/enums/RoleEnum';
+import { Actor } from 'src/app/models/actor';
 import { AuthService } from 'src/app/services/auth.service';
 import { MessageService } from 'src/app/services/message.service';
 
@@ -14,36 +15,34 @@ import { MessageService } from 'src/app/services/message.service';
 export class RegisterComponent implements OnInit {
 
   registrationForm: FormGroup;
-  roleList : string[];
 
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
     private messageService: MessageService
   ) {
-    this.roleList = this.authService.getRoles();
-    // Delete ANONYMOUS role from the list, as it is not allowed to register as ANONYMOUS
-    this.roleList.splice(this.roleList.indexOf(Role.ANONYMOUS), 1);
     this.registrationForm = this.fb.group({
       name: [''],
       surname: [''],
       email: [''],
       password: [''],
       phone: [''],
-      address: [''],
-      role: [Role.EXPLORER]
+      address: ['']
     });
   }
 
   onRegister() {
-    this.authService.registerUser(this.registrationForm.value)
+    let newUser = new Actor();
+    newUser = { ...this.registrationForm.value, role: Role.EXPLORER };
+    this.authService.registerUser(newUser)
       .then(res => {
-        this.messageService.notifyMessage('Registration successful, navitage to login page', MessageType.SUCCESS);
+        let successMsg = $localize `Registration successful, navigate to login page`
+        this.messageService.notifyMessage(successMsg, MessageType.SUCCESS);
       })
       .catch(error => {
-        let errorMsg = 'Something wrong occurred...';
+        let errorMsg = $localize `Something wrong occurred...`;
         if (error.status === 422) {
-          errorMsg = 'There are some errors in the data introduced';
+          errorMsg = $localize `There are some errors in the data introduced`;
         }
         this.messageService.notifyMessage(errorMsg, MessageType.DANGER);
       });
