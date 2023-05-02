c8648442a00e7cb3fac0f736ee6d15fb8774e367
jorandmol <jorandmol.dev@gmail.com>
2023-05-02 18:20:29 +0200

    Finish actor list component

--- src/app/components/actor/actor-list/actor-list.component.ts
+++ src/app/components/actor/actor-list/actor-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { Role } from 'src/app/enums/RoleEnum';
+import { Status } from 'src/app/enums/StatusEnum';
+import { Actor } from 'src/app/models/actor';
+import { ActorService } from 'src/app/services/actor.service';
+import { ApplicationService } from 'src/app/services/application.service';
 
 @Component({
   selector: 'app-actor-list',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActorListComponent implements OnInit {
 
-  constructor() { }
+  actors: Actor[];
+
+  @ViewChild('actorsTable') table: any;
+
+  sorts = [
+    { prop: 'Name', dir: 'desc' },
+    { prop: 'Surname', dir: 'desc' },
+    { prop: 'Email', dir: 'desc' },
+    { prop: 'Role', dir: 'desc' },
+  ];
+
+  loadingIndicator = true;
+  reorderable = true;
+  ColumnMode = ColumnMode;
+
+  constructor(
+    private actorService: ActorService,
+    private router: Router
+  ) {
+    this.actors = [];
+  }
 
   ngOnInit(): void {
+    this.actorService.getActors().subscribe(actors => {
+      this.actors = actors;
+    });
+  }
+
+  getRoleName(role: Role) {
+    return this.actorService.getRoleName(role);
+  }
+
+  displayActor(actor: Actor) {
+    this.router.navigate(['/actors', actor._id]);
   }
 
 }
--- src/app/services/actor.service.ts
+++ src/app/services/actor.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Actor } from '../models/actor';
 import { AuthService } from './auth.service';
+import { Role } from '../enums/RoleEnum';
 
 @Injectable({
   providedIn: 'root'
@@ -29,6 +30,21 @@ export class ActorService {
     const url = `${this.actorsUrl}/${actor._id}`;
     const headers = this.authService.getHeaders();
     return this.http.put<Actor>(url, actor, { headers: headers } );
+  }
+
+  getRoleName(role: Role) {
+    switch (role) {
+      case Role.EXPLORER:
+        return $localize `Explorer`;
+      case Role.SPONSOR:
+        return $localize `Sponsor`;
+      case Role.MANAGER:
+        return $localize `Manager`;
+      case Role.ADMINISTRATOR:
+        return $localize `Administrator`;
+      default:
+        return $localize `Anonymous`;
+    }
   }
 
 }
